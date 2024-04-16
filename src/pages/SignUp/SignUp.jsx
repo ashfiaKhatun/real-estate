30eaf5ad51c5c805c1f3c77efeebf07d7b3c2592
ashfiaKhatun <ashfia1204@gmail.com>
2024-04-16 23:25:30 +0600

    created basic layout

---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,70 @@
+import { Form, Link } from "react-router-dom";
+
+const SignUp = () => {
+    const submitSignUp = e => {
+        e.preventDefault();
+
+        const form = new Form(e.currentTarget);
+        const name = form.get('name');
+        const photo = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+
+        console.log(name, photo, email, password);
+    }
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center mb-6">
+                    <h1 className="text-5xl font-bold">Sign Up now!</h1>
+
+                </div>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form className="card-body" onSubmit={submitSignUp}>
+                        {/* name */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                        </div>
+
+                        {/* photo */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
+                        </div>
+
+                        {/* email */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+
+                        {/* password */}
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                    <div>
+                        <p>Already have any account? Please <Link to='/signin'><button className="btn btn-link">Sign In</button></Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    ); 
+};
+
+export default SignUp;
---
+++ src/routes/PublicRouter.jsx
@@ -0,0 +1,32 @@
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Root from "../layout/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
+import SignIn from "../pages/SignIn/SignIn";
+import SignUp from "../pages/SignUp/SignUp";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/signin',
+                element: <SignIn></SignIn>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+        ]
+    },
+]);
+
+export default router;
